fcda143693e7e563a667abe8e2acefa3f583d619
Pritish <pritish@transformhub.com>
2024-05-02 16:28:59 +0530

    first commit

---
+++ plugin/flashlight/flashlight_module.h
@@ -0,0 +1,10 @@
+#ifndef flashlight_module_h
+#define flashlight_module_h
+
+#import <Foundation/Foundation.h>
+#import "./flashlight.h"
+
+void register_flashlight_types();
+void unregister_flashlight_types();
+
+#endif /* flashlight_module_h */
---
+++ plugin/flashlight/flashlight_module.mm
@@ -0,0 +1,21 @@
+#ifdef VERSION_4_0
+#include "core/config/engine.h"
+#else
+#include "core/engine.h"
+#endif
+
+
+#include "flashlight_module.h"
+
+Flashlight * flashlight;
+
+void register_flashlight_types() {
+    flashlight = memnew(Flashlight);
+    Engine::get_singleton()->add_singleton(Engine::Singleton("Flashlight", flashlight));
+};
+
+void unregister_flashlight_types() {
+    if (flashlight) {
+        memdelete(flashlight);
+    }
+}
